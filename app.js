5ae16fddcb468b02b407b3530c4f5184925cedbd
tonmoy <tonmoy3673@gmail.com>
2025-02-25 21:15:18 +0600

    :bulb: implement input field validation

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Connected')
+console.log('Connected');
+
+const numberSubmit=(e)=>{
+    e.preventDefault();
+    let prevNum=[];
+    const form = e.target;
+    const inputField = form.guessField.value;
+    console.log(inputField)
+    if(isNaN(inputField) || inputField==''){
+        alert('Invalid Input Number!!');
+        form.guessField.value='';
+        return 0;
+    }
+
+}
