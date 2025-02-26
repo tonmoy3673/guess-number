1ec85e60a4d7a3a073532f18001fd460fad26cb9
tonmoy <tonmoy3673@gmail.com>
2025-02-26 23:46:23 +0600

    :zap: implement guess number app

--- app.js
+++ app.js
@@ -12,20 +12,27 @@ const numberSubmit=(e)=>{
     const finalResult= document.getElementById('final-result')
     const resultContainer = document.getElementById('result-container');
     const submitButton= document.getElementById('subt');
-
-    console.log(typeof(result))
+    const prevMessage= document.getElementById('guesses');
     
-
-
-    console.log(inputField)
     if(isNaN(inputField) || inputField==''){
         alert('Invalid Input Number!!');
         form.guessField.value='';
         return 0;
     }
-    else if(inputField < guessNum){
+    else{
+        prevNum.push(inputField);
+    }
+
+       
+    prevNum.join(', ');
+    prevNum.forEach(num=>{
+        prevMessage.innerText=num;
+    })
+
+     if(inputField < guessNum){
         if(result>0){
             resultValue.innerText=  result-1
+            prevNum.push(inputField)
         }
         else {
             form.guessField.disabled = true; 
@@ -38,6 +45,7 @@ const numberSubmit=(e)=>{
        
         if(result>0){
             resultValue.innerText=  result-1
+            
         }
         else {
             form.guessField.disabled = true;
@@ -49,16 +57,17 @@ const numberSubmit=(e)=>{
         }
          text.innerText=`Your Number ${inputField} is Higher Than Guess Number!!`;
         return 0;
+    
     }
+    
+
         if (inputField==guessNum) {
             resultContainer.style.display='none'
             submitButton.style.display='none';
             finalResult.style.cssText='font-size:25px; color:green;font-weight:600;'
             finalResult.innerText=`Matched!! You Win The Game!!`
         }
-    
-        prevNum.push(inputField)
-        const prevMessage= document.getElementById('guesses');
+        
 
     
 
